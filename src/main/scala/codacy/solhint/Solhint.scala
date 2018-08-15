42375c2506b22c933e2667690de662dfa5be4975
Carlos Pinho <carlosdspinho@gmail.com>
2018-08-15 15:17:50 +0100

    Parse tool result to codacy resul

--- src/main/scala/codacy/solhint/Solhint.scala
+++ src/main/scala/codacy/solhint/Solhint.scala
@@ -1,20 +1,14 @@
 package codacy.solhint
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.Paths
 
-import com.codacy.plugins.api.results.Result.Issue
+import com.codacy.plugins.api.results.Result.{FileError, Issue}
 import com.codacy.plugins.api.{Options, Source}
 import com.codacy.plugins.api.results.{Pattern, Result, Tool}
 import com.codacy.tools.scala.seed.utils.CommandRunner
-import play.api.libs.json._
 
 import scala.util.Try
-
-case class WarnResult(patternId: String, file: String, message: String, line: String = "1")
-
-object WarnResult {
-  implicit val warnResultFmt = Json.format[WarnResult]
-}
+import scala.util.matching.Regex
 
 object Solhint extends Tool {
 
@@ -29,25 +23,27 @@ object Solhint extends Tool {
     Try {
       val path = Paths.get(source.path)
       val filesToLint: Seq[String] = files.fold(Seq(source.path)) { paths =>
-        paths.map(_.toString).toSeq
+        paths.map(_.toString)(collection.breakOut)
       }
 
-      val command = List("solhint", "-f unix ") ++ filesToLint
+      val command = List("solhint", "-f","unix") ++ filesToLint
 
-      CommandRunner.exec(command) match {
+      CommandRunner.exec(command, Option(path.toFile)) match {
         case Right(resultFromTool) =>
           val output = resultFromTool.stdout ++ resultFromTool.stderr
-          parseToolResult(output, path, checkPattern(configuration))
+          parseToolResult(output)
         case Left(failure) =>
           throw failure
       }
     }
   }
 
-  private def parseToolResult(resultFromTool: List[String], path: Path, wasRequested: String => Boolean): List[Result] = ???
-
-  private def checkPattern(conf: Option[List[Pattern.Definition]])(patternId: String): Boolean = {
-    conf.forall(_.exists(_.patternId.value.toLowerCase == patternId.toLowerCase))
+  private def parseToolResult(resultFromTool: List[String]): List[Result] = {
+    val outputFormat: Regex = """(.+):(.+):(.+): (.+) \[(.+)\/(.+)\]""".r
+    resultFromTool.collect {
+      case outputFormat(fileName, line, _, description, _, pattern) =>
+        Issue(Source.File(fileName), Result.Message(description), Pattern.Id(pattern), Source.Line(line.toInt))
+    }
   }
 
 }
