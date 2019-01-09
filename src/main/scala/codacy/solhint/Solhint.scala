c8b4c19229593d2bde2669cfead9421ef7e507d1
João Lopes <jlglopes96@gmail.com>
2019-01-09 16:09:55 +0000

    Create config file from configuration

--- src/main/scala/codacy/solhint/Solhint.scala
+++ src/main/scala/codacy/solhint/Solhint.scala
@@ -1,18 +1,21 @@
 package codacy.solhint
 
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 
+import better.files.File
 import com.codacy.plugins.api.results.Result.{FileError, Issue}
 import com.codacy.plugins.api.{Options, Source}
 import com.codacy.plugins.api.results.{Pattern, Result, Tool}
 import com.codacy.tools.scala.seed.utils.CommandRunner
+import com.codacy.tools.scala.seed.utils.FileHelper._
+import play.api.libs.json._
 
 import scala.util.Try
 import scala.util.matching.Regex
 
 object Solhint extends Tool {
 
-  val configFile = ".solhint.json"
+  val configFile = Set(".solhint.json")
 
   override def apply(
     source: Source.Directory,
@@ -20,13 +23,15 @@ object Solhint extends Tool {
     files: Option[Set[Source.File]],
     options: Map[Options.Key, Options.Value]
   )(implicit specification: Tool.Specification): Try[List[Result]] = {
+
+    val configFilePath = getConfigFile(source, configuration)
     Try {
       val path = Paths.get(source.path)
       val filesToLint: Seq[String] = files.fold(Seq(source.path)) { paths =>
         paths.map(_.toString)(collection.breakOut)
       }
 
-      val command = List("solhint", "-f","unix") ++ filesToLint
+      val command = List("solhint", "-f", "unix", "--config", configFilePath.toString) ++ filesToLint
 
       CommandRunner.exec(command, Option(path.toFile)) match {
         case Right(resultFromTool) =>
@@ -35,6 +40,36 @@ object Solhint extends Tool {
         case Left(failure) =>
           throw failure
       }
+    }
+  }
+
+  def checkForExistingConfigFile(source: Source.Directory): Option[Path] = {
+    findConfigurationFile(Paths.get(source.path), configFile)
+  }
+
+  def getConfigFile(source: Source.Directory, configuration: Option[List[Pattern.Definition]]): Path = {
+    configuration.map { config =>
+      val patterns = config.map { pattern =>
+        val parameter = pattern.parameters
+          .flatMap(_.headOption.map { param =>
+            val parameterValue: JsValue = param.value
+            parameterValue
+          })
+          .getOrElse(JsNull)
+
+        (pattern.patternId.value, parameter)
+      }
+      File
+        .newTemporaryFile(".solhint", ".json")
+        .write(Json.prettyPrint(Json.toJson(JsObject(Seq(("rules", JsObject(patterns)))))))
+        .path
+    }.orElse {
+      checkForExistingConfigFile(source)
+    }.getOrElse {
+      File
+        .newTemporaryFile(".solhint", ".json")
+        .write(Json.prettyPrint(Json.toJson(JsObject(Seq(("extends", JsString("default")))))))
+        .path
     }
   }
 
